906b66e6afaa814031eaec6170d808e9d41002d2
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2025-02-03 19:34:46 +0530

    Array operations in c

---
+++ data_strucutres/3_arrayops.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    printf("1. Insert\n");
+    printf("2. Delete\n");
+    printf("3. Search\n");
+    printf("4. Update\n");
+    printf("5. Display\n");
+    printf("6. Exit\n");
+
+
+}
