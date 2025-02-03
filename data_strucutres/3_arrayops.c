6fc785da97ef07ca8a3692db5d097b82bdfdac55
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2025-02-03 19:53:13 +0530

    Handle user input

--- data_strucutres/3_arrayops.c
+++ data_strucutres/3_arrayops.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 
 int main() {
-    printf("1. Insert\n");
-    printf("2. Delete\n");
-    printf("3. Search\n");
-    printf("4. Update\n");
-    printf("5. Display\n");
-    printf("6. Exit\n");
+    int option;
 
+    while(1) {
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Search\n");
+        printf("4. Update\n");
+        printf("5. Display\n");
+        printf("6. Exit\n");
 
+        printf("Choose an option: ");
+        scanf("%d", &option);
+
+        switch (option) {
+            case 1:
+                printf("Insert operation selected.\n");
+                break;
+            case 2:
+                printf("Delete operation selected.\n");
+                break;
+            case 3:
+                printf("Search operation selected.\n");
+                break;
+            case 4:
+                printf("Update operation selected.\n");
+                break;
+            case 5:
+                printf("Display operation selected.\n");
+                break;
+            case 6:
+                printf("Exiting program...\n");
+                return 0;
+            default:
+                printf("Invalid option! Please choose between 1 and 6.\n");
+        }
+    }
+    
+    return 0;
 }
