6f1aab0583f7427a1718b288d69873209e8d991f
Nithin V <nithinvblr@gmail.com>
2025-02-08 17:40:39 +0530

    Experiment with reserve

--- data_strucutres/4_vectorcap.cpp
+++ data_strucutres/4_vectorcap.cpp
@@ -4,7 +4,8 @@
 int main() {
     std::vector<int> arr;
 
-    for (int i = 1; i <= 10; i++) {
+    arr.reserve(128);
+    for (int i = 1; i <= 150; i++) {
         arr.push_back(i);
         std::cout << "Size: " << arr.size() << ", Capacity: " << arr.capacity() << std::endl;
     }
