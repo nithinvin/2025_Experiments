2eb2d1b3929545957df4f2939a5ffb6e4adb403c
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2025-02-18 20:26:32 +0530

    Find min, max and average grades from vector

--- data_strucutres/5_vectors.cpp
+++ data_strucutres/5_vectors.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     vector<int> grades;
-    int grade;
+    int grade, max = 0, min = 0, total_grade = 0;
 
     while (true) {
         cout << "Enter grade (type -1 to stop): ";
@@ -16,8 +16,16 @@ int main() {
         // cout << "Vector size: " << grades.size() << endl;
         // cout << "Vector capacity: " << grades.capacity() << endl;
     }
+    max = min = grades[0];
     for (auto it = grades.begin(); it != grades.end(); ++it){
         cout << *it << " ";
+        if (max < *it)
+            max = *it;
+        if (min > *it)
+            min = *it;
+        total_grade += *it;
     }
-    cout << endl;	
+    cout << "Largest grade is " << max << " and smallest grade is " << min << endl;
+    cout << "Average grade is " << total_grade/grades.size();
+    cout << endl;
 }
