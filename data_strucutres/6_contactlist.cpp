0262d4cea6f6165faf80f641af2015c136eb54c8
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2025-02-24 21:36:28 +0530

    display list of contacts

--- data_strucutres/6_contactlist.cpp
+++ data_strucutres/6_contactlist.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <list>
+#include <sstream>
 
 using namespace std;
 
 int main() {
+    list<pair<string, string>> contacts;
     ifstream file("contact_numbers.csv");
     
     if(!file) {
         cout << "Error: unable to open file!" << endl;
         return 1;
     }
+
+    string line, name, phone;
+
+    while (getline(file, line)) {
+        stringstream ss(line); 
+        getline(ss, name, ','); 
+        getline(ss, phone, '\n');
+        contacts.push_back(make_pair(name, phone));
+    }
+
     file.close();
+
+    cout << "Contacts List:" << endl;
+    for (const auto& contact : contacts) {
+        cout << contact.first << " - " << contact.second << endl;
+    }
 
     return 0;
 }
