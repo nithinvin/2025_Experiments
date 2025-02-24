3990afdeb39f34db9242abb74d987fbf16e86c72
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2025-02-24 21:13:21 +0530

    open designated file

--- data_strucutres/6_contactlist.cpp
+++ data_strucutres/6_contactlist.cpp
@@ -1,11 +1,16 @@
-//#include <iostream>
+#include <iostream>
 #include <fstream>
 
 using namespace std;
 
 int main() {
     ifstream file("contact_numbers.csv");
-
+    
+    if(!file) {
+        cout << "Error: unable to open file!" << endl;
+        return 1;
+    }
+    file.close();
 
     return 0;
 }
