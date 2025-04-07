28ab8ebba576812cc921cdef84dff008969c2407
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2025-04-07 16:13:48 +0530

    More socket experiments

--- network_programs/sockets.c
+++ network_programs/sockets.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/socket.h>
 
 int main() {
     int socket_fd;
+    int iter = 0;
 
-    while(1) {
+    while(iter < 1000) {
         socket_fd = socket(AF_INET, SOCK_STREAM, 0);
         if (socket_fd < 0) {
             perror("socket");
             exit(EXIT_FAILURE);
         }
         printf("socket_fd %d\n", socket_fd);
+        ++iter;
+        // close(socket_fd);
     }
 }
