21a0b1c43d2bebb30a412192399d157e4cff4b4a
Nithin Vinayagamoorthy <nithinvblr@gmail.com>
2025-04-17 18:07:46 +0530

    print client address and port number

--- basic_web_servers/httpserver.py
+++ basic_web_servers/httpserver.py
@@ -2,10 +2,12 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        client_ip, client_port = self.client_address
+        print(f"Client connected from {client_ip}:{client_port}")
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello from Nithin's Python Web Server!")
 
-server = HTTPServer(('172.22.91.41', 8080), SimpleHandler)
-print("Server started on http://172.22.91.41:8080")
+server = HTTPServer(('172.22.91.41', 9090), SimpleHandler)
+print("Server started on http://172.22.91.41:9090")
 server.serve_forever()
