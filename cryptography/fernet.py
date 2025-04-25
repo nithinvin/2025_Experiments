993ed96846970fd54eaa64cd68f1ef852b91b249
nithinvin <nithinvblr@gmail.com>
2025-04-25 17:00:44 +0530

    encrypt a short word

--- cryptography/fernet.py
+++ cryptography/fernet.py
@@ -4,7 +4,8 @@ key = Fernet.generate_key()
 print("key is", key)
 f = Fernet(key)
 
-token = f.encrypt(b"My secret message")
+#token = f.encrypt(b"My secret message")
+token = f.encrypt(b"My")
 print("Encrypted:", token)
 
 print("Decrypted:", f.decrypt(token))
