7ee75d5c9e4700d8494e4dd08b6f02960202a875
nithinvin <nithinvblr@gmail.com>
2025-05-11 18:57:37 +0530

    c++ basics

---
+++ cpp_udemy/basics.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter 2 numbers:";
+    cin>>a>>b;
+    c = a + b;
+    cout<<"The sum is:"<<c<<endl;
+    return 0;
+}
